c2d27b03458092e837f01f4ac8faa01528325399
mahesh-changale <maheshchangale5@gmail.com>
2023-09-21 15:09:04 +0530

    End of section 3 and complete angular setup

--- API/Program.cs
+++ API/Program.cs
@@ -16,7 +16,11 @@ builder.Services.AddDbContext<DataContext>(option =>{
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
